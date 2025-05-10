e1ee7fba8fcbb2e8e4a5420d34c01e1fd84e101f
p0dkan <podkanowiczpiotr@gmail.com>
2025-05-10 14:57:49 +0200

    interakcje

--- src/App.js
+++ src/App.js
@@ -4,31 +4,64 @@ import {useState} from "react";
 
 function App() {
 
-    const [email, setEmail] = useState('a');
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false)
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
-    let message;
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>;
-    } else if (email.length < 15) {
-        message = <div>Twój adres e-mail jest w sam raz.</div>;
-    } else {
-        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    function handleSubmit() {
+        setSubmitted(true);
+    }
+
+    function handleLogOut(){
+        setSubmitted(false)
 
     }
-    return (
-        <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}</h2>
-            <h6>{message}</h6>
-            <input type="text" value={email} onChange={handleChange}/>
-
-        </div>
 
 
-    );
+    // let message;
+    // if (email.length < 10) {
+    //     message = <div>Ale masz krótki adres!</div>;
+    // } else if (email.length < 15) {
+    //     message = <div>Twój adres e-mail jest w sam raz.</div>;
+    // } else {
+    //     message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    //
+    // }
+
+    if (submitted){
+        return (
+
+            <div>
+                <h1>Witaj w systemie do zapisów na zajęcia</h1>
+                <h1>Witaj, {email}!</h1>
+                <button type="button" onClick={handleLogOut}>
+                    wyloguj
+                </button>
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <div>
+                <h1>Witaj w systemie do zapisów na zajęcia</h1>
+                <label>
+                    Zaloguje się e-mailem:
+                    <input type="text" value={email} onChange={handleChange} />
+                </label>
+                <button type="button" onClick={handleSubmit}>
+                    Wchodzę
+                </button>
+
+            </div>
+
+        );
+
+    }
+
+
 }
 export default App;
