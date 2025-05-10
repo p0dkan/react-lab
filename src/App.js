f0cf6a381da590dd6ce10ed6424a6aebbf4d60d8
p0dkan <podkanowiczpiotr@gmail.com>
2025-05-10 14:03:02 +0200

    reaktywność

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
 
-    let email = 'fracz@agh.edu.pl';
-    function handleChange(event) {
+    const [email, setEmail] = useState('a');
 
-        console.log(event.target.value);
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+
     }
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <h6>{message}</h6>
+            <input type="text" value={email} onChange={handleChange}/>
+
         </div>
+
+
     );
 }
 export default App;
