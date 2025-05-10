5c0fd7a38a7f794d2b8160e33cb8b8a0777befab
p0dkan <podkanowiczpiotr@gmail.com>
2025-05-10 15:41:47 +0200

    loogowanie - weryfikacja maila

--- src/App.js
+++ src/App.js
@@ -1,67 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
+import "milligram";
 
 function App() {
 
     const [email, setEmail] = useState('');
-    const [submitted, setSubmitted] = useState(false)
+    const [submitted, setSubmitted] = useState(false);
+    const [validEmail, setValidEmail] = useState(true);
 
     function handleChange(event) {
         setEmail(event.target.value);
+        setValidEmail(true);
     }
 
-    function handleSubmit() {
-        setSubmitted(true);
+    function validateEmail(email) {
+        return email.includes('@');
     }
 
-    function handleLogOut(){
-        setSubmitted(false)
+    function handleSubmit(event) {
+        event.preventDefault();
 
+        if (validateEmail(email)) {
+            setSubmitted(true);
+        } else {
+            setValidEmail(false);
+        }
     }
 
+    function handleLogOut() {
+        setSubmitted(false);
+        setEmail('');
+        setValidEmail(true);
+    }
 
-    // let message;
-    // if (email.length < 10) {
-    //     message = <div>Ale masz krótki adres!</div>;
-    // } else if (email.length < 15) {
-    //     message = <div>Twój adres e-mail jest w sam raz.</div>;
-    // } else {
-    //     message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
-    //
-    // }
-
-    if (submitted){
+    if (submitted) {
         return (
-
             <div>
                 <h1>Witaj w systemie do zapisów na zajęcia</h1>
                 <h1>Witaj, {email}!</h1>
                 <button type="button" onClick={handleLogOut}>
-                    wyloguj
+                    Wyloguj
                 </button>
             </div>
         );
-    }
-
-    else {
+    } else {
         return (
             <div>
                 <h1>Witaj w systemie do zapisów na zajęcia</h1>
-                <label>
-                    Zaloguje się e-mailem:
-                    <input type="text" value={email} onChange={handleChange} />
-                </label>
-                <button type="button" onClick={handleSubmit}>
-                    Wchodzę
-                </button>
-
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Zaloguj się e-mailem:
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    {!validEmail && (
+                        <p className="error">
+                            Adres e-mail musi zawierać znak "@"
+                        </p>
+                    )}
+                    <button type="submit">
+                        Wchodzę
+                    </button>
+                </form>
             </div>
-
         );
-
     }
-
-
 }
+
 export default App;
